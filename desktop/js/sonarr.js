78d8d44a7635e39d3d7fa6ee5476f4db921111df
hbedek <henri.besses@gmail.com>
2021-09-09 10:51:48 +0200

    Update sonarr.js

--- desktop/js/sonarr.js
+++ desktop/js/sonarr.js
@@ -61,6 +61,7 @@ function addCmdToTable(_cmd) {
     tr += '<a class="btn btn-default btn-xs cmdAction" data-action="configure"><i class="fas fa-cogs"></i></a> ';
     tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fas fa-rss"></i> Tester</a>';
   }
+  tr += '<i class="fas fa-minus-circle pull-right cmdAction cursor" data-action="remove"></i></td>';
   tr += '</tr>';
   $('#table_cmd tbody').append(tr);
   var tr = $('#table_cmd tbody tr').last();
